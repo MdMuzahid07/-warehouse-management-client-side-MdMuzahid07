2edcbf66bd775a013024fbd0ac8807b5e9bd1577
MdMuzahid07 <MdMuzahid7396@gmail.com>
2022-05-09 00:45:07 +0600

    trying to fix post error

--- src/components/ManageItems/ManageItems.js
+++ src/components/ManageItems/ManageItems.js
@@ -3,32 +3,59 @@ import { useForm } from 'react-hook-form';
 import './ManageItems.css';
 
 const ManageItems = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
 
-    console.log(watch("example")); // watch input value by passing the name of it
+
+
+
+
+
+    const handleOnSubmit = event => {
+
+        event.preventDefault();
+
+        const name = event.target.name.value;
+        const description = event.target.description.value;
+        const picture = event.target.picture.value;
+        const price = event.target.picture.value;
+        const supplier = event.target.supplier.value;
+
+        const data = {name, description, picture, price, supplier};
+
+
+
+        const url = `http://localhost:5000/product`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+            .then(product => {
+
+            })
+
+    };
+
 
     return (
         <div className='container my-5 p-3 rounded rounded'>
-            <div className='w-75 mx-auto bg-light'>
+            <div className='w-75 mx-auto bg-dark'>
                 <div className="row">
                     <div className="col-md-6">
                         <img className='w-100 bg-warning p-5' src="https://cdn3.iconfinder.com/data/icons/web-development-66/64/z-123-512.png" alt="" />
                     </div>
-                    <div className="col-md-6 bg-light p-3 text-white">
-                        <h4 className='text-warning'>Upload Products</h4>
+                    <div className="col-md-6 bg-dark p-3 text-white">
+                        <h4 className='text-warning  mt-3'>Upload Products</h4>
+                        <form className='d-flex flex-column' onSubmit={handleOnSubmit}>
+                            <input className='border-0 p-1 my-2 rounded' type="text" name='name' placeholder="product-name" id="" required />
+                            <input className='border-0 p-1 mb-2 rounded' type="text" name='description' placeholder="product-description" id="" required />
+                            <input className='border-0 p-1 mb-2 rounded' type="text" name='picture' placeholder="product-picture" id="" required />
+                            <input className='border-0 p-1 mb-2 rounded' type="number" name='price' placeholder="product-price" id="" required />
+                            <input className='border-0 p-1 mb-2 rounded' type="text" name='supplier' placeholder="product-supplier" id="" required />
 
-
-                        <form onSubmit={handleSubmit(onSubmit)}>
-                            {/* register your input into the hook by invoking the "register" function */}
-                            <input defaultValue="test" {...register("example")} />
-
-                            {/* include validation with required or other standard HTML validation rules */}
-                            <input {...register("exampleRequired", { required: true })} />
-                            {/* errors will return when field validation fails  */}
-                            {errors.exampleRequired && <span>This field is required</span>}
-
-                            <input type="submit" />
+                            <button className='button border-0 rounded' type='submit'>Add Product</button>
                         </form>
                     </div>
                 </div>
